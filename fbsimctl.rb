2b117fb1d508242e806c3913dc1e9d97c92a4079
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-12-27 13:13:28 +0100

    Patch formula

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -3,7 +3,7 @@ class Fbsimctl < Formula
   homepage "https://github.com/facebook/FBSimulatorControl/fbsimctl/README.md"
   url "https://github.com/facebook/FBSimulatorControl/tarball/v0.2.2"
   sha256 "119718ee8a0611d087f46dda1603c6622b075270ef47889c2b16d781391e7c2b"
-  head "https://github.com/facebook/FBSimulatorControl.git"
+  head "https://github.com/Ruenzuo/FBSimulatorControl.git"
 
   depends_on "carthage"
   depends_on :xcode => ["8", :build]
